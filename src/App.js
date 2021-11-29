8e0fa320df030dd81e9a95b8a4d753cf4b071c6b
J. Francisco Martín <jfmartin@soincon.es>
2021-11-29 18:51:53 +0100

    chore: Add prettier and eslint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 const App = () => {
-
   const darkTheme = createTheme({
     palette: {
       mode: 'dark',
@@ -15,23 +14,21 @@ const App = () => {
   });
 
   return (
-    <React.Fragment>
     <ThemeProvider theme={darkTheme}>
-    <CssBaseline />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <CssBaseline />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </ThemeProvider>
-    </React.Fragment>
   );
-}
+};
 
 export default App;
